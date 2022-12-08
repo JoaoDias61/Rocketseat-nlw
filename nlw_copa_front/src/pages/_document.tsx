9a5e68ed3ea2840a03b67afdad637d88efe3b6d9
JoaoDias61 <jpdias641@gmail.com>
2022-12-08 12:14:48 -0300

    add asssets

---
+++ nlw_copa_front/src/pages/_document.tsx
@@ -0,0 +1,15 @@
+
+import { Html, Head, Main, NextScript } from "next/document"
+
+export function Document() {
+    return (
+        <Html>
+            <Head>
+
+            </Head>
+            <body>
+                <Main />
+            </body>
+        </Html>
+    );
+}
