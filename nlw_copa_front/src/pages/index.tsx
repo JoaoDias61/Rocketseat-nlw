cf84b59c09e1d4c1f63c9189ed22533789d9992f
JoaoDias61 <jpdias641@gmail.com>
2022-12-11 11:11:07 -0300

    adjusted tailwind

--- nlw_copa_front/src/pages/index.tsx
+++ nlw_copa_front/src/pages/index.tsx
@@ -2,24 +2,40 @@
 // JSX - JavaScript + XML (HTML)
 // TSX - TypeScript + JSX
 
-interface HomeProps {
-  count: number;
-}
+// interface HomeProps {
+//   count: number;
+// }
 
-export default function Home(props: HomeProps) {
+import Image from "next/image"
+import logoImg from "../assets/logo.svg"
+import appPreviewImg from "../assets/nlw_copa_preview.png"
+
+export default function Home() {
 
   return (
-    <h1 className="text-violet-500">Contagem: {props.count}</h1>
+    <div>
+      <main>
+        <Image src={logoImg} alt="NLW_copa" />
+        <h1>
+          Crie seu próprio bolão da copa e compartilhe entre amigos!
+        </h1>
+      </main>
+      <Image
+        src={appPreviewImg}
+        alt="Dois celulares exibindo um preview da aplicação nlw"
+        quality={100}
+      />
+    </div>
   )
 }
 
-export const getServerSideProps = async () => {
-  const response = await fetch("http://localhost:2222/pools/count")
-  const data = await response.json()
+// export const getServerSideProps = async () => {
+//   const response = await fetch("http://localhost:2222/pools/count")
+//   const data = await response.json()
 
-  return {
-    props: {
-      count: data.pools
-    }
-  }
-}
+//   return {
+//     props: {
+//       count: data.pools
+//     }
+//   }
+// }
