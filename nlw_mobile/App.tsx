c8a69c9c54d516c434c273d51dccc5c6f6d20a5b
JoaoDias61 <jpdias641@gmail.com>
2022-12-13 11:30:39 -0300

    authentication using google

--- nlw_mobile/App.tsx
+++ nlw_mobile/App.tsx
@@ -5,6 +5,7 @@ import { THEME } from "./src/styles/theme"
 import React from 'react';
 import { Loading } from './src/components/Loading';
 import { Signin } from './src/screens/Signin';
+import { AuthContext, AuthContextProvider } from './src/context/AuthContext';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_500Medium, Roboto_700Bold })
@@ -12,14 +13,16 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      {
-        fontsLoaded ? <Signin /> : <Loading />
-      }
+      <AuthContextProvider>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        {
+          fontsLoaded ? <Signin /> : <Loading />
+        }
+      </AuthContextProvider>
     </NativeBaseProvider>
   );
 }
--- nlw_mobile/src/screens/Signin.tsx
+++ nlw_mobile/src/screens/Signin.tsx
@@ -1,12 +1,28 @@
-import { Center, Text } from "native-base";
+import { Center, Icon, Text } from "native-base";
+import { Fontisto } from "@expo/vector-icons"
+import { Button } from "../components/Button";
+import { useAuth } from "../hooks/useAuth"
+
+import Logo from "../assets/logo.svg"
 
 interface SigninProps { }
 
 export function Signin(props: SigninProps) {
+
+    const { signIn, user } = useAuth();
+
     return (
-        <Center flex={1} bgColor="gray.900" >
-            <Text color="white" fontSize={24} fontFamily="heading">
-                Signin
+        <Center flex={1} bgColor="gray.900" p={7}>
+            <Logo width={212} height={40} />
+            <Button
+                type="SECONDARY"
+                title="ENTRAR COM O GOOGLE"
+                leftIcon={<Icon as={Fontisto} name="google" color="white" size="md" />}
+                mt={12}
+                onPress={signIn}
+            />
+            <Text color="white" textAlign="center" mt={4}>
+                Não utilizamos nenhuma informação além do seu {'\n'} e-mail para criação de sua conta.
             </Text>
         </Center>
     );
